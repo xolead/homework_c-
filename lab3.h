b43dd212fe690f6c751421c941649a5eab63c989
xolead <144803325+xolead@users.noreply.github.com>
2024-11-08 12:27:47 +0500

    Update lab3.h

--- lab3.h
+++ lab3.h
@@ -3,3 +3,36 @@
 
 class Person;
 int myIntInput();
+
+
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+class QuadraticEquation
+{
+  public:
+    double a,b,c;
+    
+    double discrim()
+    {
+        return (b*b)-(4*a*c);
+    }
+    
+    
+    
+    void print()
+    {
+        cout<<discrim();
+    }
+};
+
+int main()
+{
+    QuadraticEquation abc(1,4,4);
+    abc.print();
+    return 0;
+}
